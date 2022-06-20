82763f9b8b0bca5169c903bb7af2c3da455aaa51
ambroise <ambroise.berthe@ikomia.com>
2022-06-20 17:52:42 +0200

    Add test file

---
+++ train_torchvision_mask_rcnn_test.py
@@ -0,0 +1,16 @@
+import logging
+from ikomia.core import task, ParamMap
+from ikomia.utils.tests import run_for_test
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train torchvision mask rcnn =====")
+    input_dataset = t.getInput(0)
+    params = task.get_parameters(t)
+    params["epochs"] = 2
+    params["batch_size"] = 1
+    task.set_parameters(t, params)
+    input_dataset.load(data_dict["datasets"]["segmentation"]["dataset_coco"])
+    yield run_for_test(t)
