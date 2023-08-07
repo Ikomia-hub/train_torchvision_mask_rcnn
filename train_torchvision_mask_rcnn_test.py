f3e7abe16c7b6044250c9045bb239de4217d64f6
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 17:44:01 +0200

    Fix test file

--- train_torchvision_mask_rcnn_test.py
+++ train_torchvision_mask_rcnn_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,9 +7,9 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train torchvision mask rcnn =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
-    params["epochs"] = 2
-    params["batch_size"] = 1
-    task.set_parameters(t, params)
+    params = t.get_parameters()
+    params["epochs"] = "2"
+    params["batch_size"] = "1"
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["instance_segmentation"]["dataset_coco"])
     yield run_for_test(t)
